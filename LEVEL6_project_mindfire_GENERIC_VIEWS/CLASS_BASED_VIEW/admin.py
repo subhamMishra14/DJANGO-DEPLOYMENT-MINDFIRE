26e0e6047ca082277ae11f7a0158fafd1885d445
subham mishra <subham.mishra14@gmail.com>
2019-12-17 21:33:54 +0530

    6th Project

---
+++ LEVEL6_project_mindfire_GENERIC_VIEWS/CLASS_BASED_VIEW/admin.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+from django.contrib import admin
+
+# Register your models here.
+=======
+from django.contrib import admin
+
+# Register your models here.
+>>>>>>> e130dfaa52201b6bb61049d31118551875f3a902
---
+++ LEVEL6_project_mindfire_GENERIC_VIEWS/CLASS_BASED_VIEW/models.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+from django.db import models
+
+# Create your models here.
+=======
+from django.db import models
+
+# Create your models here.
+>>>>>>> e130dfaa52201b6bb61049d31118551875f3a902
